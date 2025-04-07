ed9e9097a1debdbdc584e25ca8f9c8f87b87bcb4
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2025-04-07 14:46:45 +0530

    chore: update ESLint and Prettier config, lint entire codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import prettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,32 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: [".github/", "node_modules/", ".next/", "src/components/ui", "*.config.ts", "*.mjs"] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      prettier: prettier,
+    },
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Prettier integration rules
+      "prettier/prettier": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
