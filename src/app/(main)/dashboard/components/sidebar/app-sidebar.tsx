88be44f9cce39427ed4855b569ff55652ac057de
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2025-05-11 16:23:53 +0530

    feat: add dashboard route and apply minor structural updates

---
+++ src/app/(main)/dashboard/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import * as React from "react";
+import { IconHelp, IconInnerShadowTop, IconSearch, IconSettings } from "@tabler/icons-react";
+
+import { NavMain } from "@/app/(main)/dashboard/components/sidebar/nav-main";
+import { NavSecondary } from "@/app/(main)/dashboard/components/sidebar/nav-secondary";
+import { NavUser } from "@/app/(main)/dashboard/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { sidebarItems } from "@/navigation/sidebar/sidebar-items";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: IconHelp,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: IconSearch,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={sidebarItems} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ src/app/(main)/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+import { cookies } from "next/headers";
+
+import { AppSidebar } from "./components/sidebar/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+import { SiteHeader } from "./components/sidebar/site-header";
+
+interface LayoutProps {
+  readonly children: ReactNode;
+}
+
+export default async function Layout({ children }: LayoutProps) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
